1c5274c644772196ddeaf74e548658565e07530c
Arian <ari7cr2@gmail.com>
2023-12-03 04:59:10 +0100

    day1

---
+++ ari_testing.py
@@ -0,0 +1,9 @@
+#import numpy as np
+
+#celsius_q = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
+#fahrenheit_a = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
+
+#for i,c in enumerate(celsius_q):
+#  print("{} degrees Celsius = {} degrees Fahrenheit".format(c, fahrenheit_a[i]))
+
+
